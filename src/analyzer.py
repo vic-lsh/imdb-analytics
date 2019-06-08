76b38ef3aa7b691d65f8aea46dbaea223a899f24
vic-lee <viclee@nyu.edu>
2019-06-08 16:23:22 -0400

    Refactor functions

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,8 +1,11 @@
+import functools
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 from constants import IMDb_Constants as consts
+from series_ratings import SeriesRatings
 
 
 class IMDb_Analyzer():
@@ -16,8 +19,46 @@ class IMDb_Analyzer():
     def __del__(self):
         self.__driver.close()
 
-    def query(self, name: str) -> None:
-        """Query a TV series's ratings with its name"""
+    def execute_in_series_homepage(func):
+        # try:
+        #     series_header = self.__driver.find_element_by_css_selector(
+        #         consts.SERIES_HEADER_CSL
+        #     )
+        #     assert series_name in series_header.text
+        # except NoSuchElementException:
+        #     print("Series title not found")
+        def wrapper(*args, **kwargs):
+            print(args)
+            func(*args, **kwargs)
+        return wrapper
+
+    def query(self, series_name: str) -> None:
+        """Query a TV series's ratings with its name.
+        Return: a SeriesRating object
+        """
+        self._navigate_to_series(series_name)
+        overall_rating = self._get_overall_rating(series_name)
+        seasons_count = self._get_seasons_count(series_name)
+        series_ratings = SeriesRatings(overall_rating=overall_rating,
+                                       seasons_count=seasons_count)
+        print(series_ratings)
+
+    # @execute_in_series_homepage
+    def _get_seasons_count(self, series_name: str) -> int:
+        seasons_count_raw = self.__driver.find_element_by_css_selector(
+            consts.SEASONS_COUNT_CSL
+        )
+        seasons_count = int(seasons_count_raw.text)
+        return seasons_count
+
+    def _get_overall_rating(self, series_name: str) -> float:
+        overall_rating_raw = self.__driver.find_element_by_css_selector(
+            consts.OVERALL_RATINGS_CSL
+        )
+        overall_rating = float(overall_rating_raw.text)
+        return overall_rating
+
+    def _navigate_to_series(self, name: str):
         self.__driver.get(consts.HOMEPAGE_URL)
         serach_bar = self.__driver.find_element_by_css_selector(
             consts.SEARCH_BAR_CSL
