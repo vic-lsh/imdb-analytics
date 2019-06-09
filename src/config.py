62cbb215e08b8070cbfc283e6f5eb9b5def24bf2
vic-lee <viclee@nyu.edu>
2019-06-09 09:40:05 -0400

    Add serialization-related configs

--- src/config.py
+++ src/config.py
@@ -9,17 +9,43 @@ class AnalyzerConfig:
         pass
 
     def __init__(self, fname=CONFIG_FNAME):
+        self.__tv_series_names = []
+        self.__headless = True
+        self.__serialization = True
+        self.__serialization_fname = "outfile"
+
         with open(fname, 'r') as cfgfile:
             cfg = yaml.safe_load(cfgfile)
+
         if 'tv_series' not in cfg:
             self.__tv_series_names = []
             raise TVSeriesUndefinedException
         else:
             self.__tv_series_names = cfg['tv_series']
+
         if 'headless' in cfg:
             self.__headless = cfg['headless']
+
+        if 'pickle' in cfg:
+            if 'should_pickle' in cfg['pickle']:
+                self.__serialization = cfg['pickle']['should_pickle']
+            if 'pickle_filename' in cfg['pickle']:
+                self.__serialization_fname = cfg['pickle']['pickle_filename']
+
+    @property
+    def should_serialize(self) -> bool:
+        """Returns if serialization is on or off, default to on if not 
+        specified in config."""
+        return self.__serialization
+
+    @property
+    def serialization_filename(self) -> str:
+        """Returns the filename used for serialization, default to None if 
+        serialization is off."""
+        if not self.__serialization:
+            return None
         else:
-            self.__headless = True
+            return self.serialization_filename
 
     @property
     def tv_series_names(self):
