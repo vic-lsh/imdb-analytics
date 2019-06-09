24a42d873447e812922f2ce44eea1c6fbeb52a38
vic-lee <viclee@nyu.edu>
2019-06-09 09:14:24 -0400

    Update title bar formatting

--- src/ratings.py
+++ src/ratings.py
@@ -42,17 +42,18 @@ class SeriesRatings():
 
     def __repr__(self):
         reprs = []
+        DESC_STR = ("Overall rating: {}\t".format(self.__OVERALL_RATING) +
+                    "Seasons count:  {}".format(self.__SEASONS_COUNT))
         HEADER_STR = "      " + "".join(["E{:<4}".format(ep_num)
                                          for ep_num in range(self.__max_episode_num)])
         SPACE_LEN = 2
-        banner_len = (len(HEADER_STR) -
+        banner_len = (max(len(HEADER_STR), len(DESC_STR)) -
                       len(self.__SERIES_NAME) - 2 * SPACE_LEN) // 2
         if banner_len < 2:
             banner_len = 2
         reprs.append("*" * banner_len + " " * SPACE_LEN +
                      self.__SERIES_NAME + " " * SPACE_LEN + "*" * banner_len)
-        reprs.append("Overall rating: {}\t".format(self.__OVERALL_RATING) +
-                     "Seasons count:  {}".format(self.__SEASONS_COUNT))
+        reprs.append(DESC_STR)
         reprs.append(HEADER_STR)
         for season_num, ratings in self.__episode_ratings.items():
             reprs.append("S{:<5}".format(season_num) +
