3b80ba047a0d2c2a0093edbf876bb10f25eaf99b
vic-lee <viclee@nyu.edu>
2019-06-08 21:03:31 -0400

    Implement query episode method

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,10 +1,17 @@
+from datetime import datetime
 import functools
 import sys
+import time
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
 from constants import IMDb_Constants as consts
 from series_ratings import SeriesRatings
@@ -12,6 +19,12 @@ from series_ratings import SeriesRatings
 
 class IMDb_Analyzer():
     """Analyzes TV series based on data from IMDb"""
+
+    class ElementStalessTimeoutException:
+        """Exception raised when element remains stale (accessing it raises 
+        StaleElementReferenceException) for `timeout` seconds.
+        """
+        pass
 
     class _Decorators():
         def execute_in_series_homepage(func):
@@ -28,20 +41,23 @@ class IMDb_Analyzer():
 
             def wrapper(self, *args, **kwargs):
                 class NoSeriesNameAsFirstArgException(Exception):
-                    """To use the `catch_no_such_element_exception` decorator,
-                    the wrapped function must provide `series_name` as its first argument.
+                    """To use the `catch_no_such_element_exception` decorator, 
+                    the wrapped function must provide `series_name` as its 
+                    first argument.
                     """
 
                     def __init__(self, message=None, payload=None):
-                        self.message = "Please provide `series name` as your first argument."
+                        self.message = "Please provide `series name`"\
+                            + " as your first argument."
                         self.payload = payload
 
                     def __str__(self):
                         return str(self.message)
                 try:
-                    series_header = self._IMDb_Analyzer__driver.find_element_by_css_selector(
-                        consts.SERIES_HEADER_CSL
-                    )
+                    series_header = self._IMDb_Analyzer__driver\
+                        .find_element_by_css_selector(
+                            consts.SERIES_HEADER_CSL
+                        )
                     if len(args) >= 1:
                         assert args[0] in series_header.text
                     else:
@@ -49,6 +65,18 @@ class IMDb_Analyzer():
                 except NoSuchElementException:
                     print("Series title not found")
                     sys.exit(1)
+                return func(self, *args, **kwargs)
+            return wrapper
+
+        def execute_in_series_episode_guide(func):
+            """Decorator that enforces the wrapped function to be executed
+            when the driver is on the Episode Guide page.
+            """
+
+            def wrapper(self, *args, **kwargs):
+                assert (consts.EPISODES_GUIDE_IDENTIFIER
+                        in self._IMDb_Analyzer__driver.current_url), \
+                    "Driver is not currently on the Episodes Guide page"
                 return func(self, *args, **kwargs)
             return wrapper
 
@@ -63,7 +91,7 @@ class IMDb_Analyzer():
 
     def __init__(self):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         self.__driver = webdriver.Chrome(options=chrome_options)
 
     def __del__(self):
@@ -79,6 +107,32 @@ class IMDb_Analyzer():
         series_ratings = SeriesRatings(overall_rating=overall_rating,
                                        seasons_count=seasons_count)
         self._navigate_to_episode_guide(series_name)
+        self._query_episode_ratings()
+
+    @_Decorators.catch_no_such_element_exception
+    @_Decorators.execute_in_series_episode_guide
+    def _query_episode_ratings(self) -> None:
+        season_dropdown = Select(self.__driver.find_element_by_css_selector(
+            consts.SEASONS_DROPDOWN_CSL
+        ))
+        seasons = season_dropdown.options
+        for index in range(0, len(seasons) - 1):
+            TIMEOUT_SECS = 10
+            start_time = datetime.now()
+            while True:
+                try:
+                    season_dropdown_elem = self.__driver.find_element_by_id(
+                        consts.SEASONS_DROPDOWN_ID
+                    )
+                    season_dropdown = Select(season_dropdown_elem)
+                    season_dropdown.select_by_index(index)
+                    break
+                except StaleElementReferenceException:
+                    continue
+                finally:
+                    if (datetime.now() - start_time).seconds > TIMEOUT_SECS:
+                        raise ElementStalessTimeoutException
+                        break
 
     @_Decorators.catch_no_such_element_exception
     @_Decorators.execute_in_series_homepage
@@ -113,7 +167,7 @@ class IMDb_Analyzer():
         serach_bar.clear()
         serach_bar.send_keys(name)
         serach_bar.send_keys(Keys.ENTER)
-        assert "Find - IMDb" in self.__driver.title
+        # assert "Find - IMDb" in self.__driver.title
         first_result_box = self.__driver.find_element_by_css_selector(
             consts.SEARCH_RESULT_FIRST_FULL_BOX_CSL
         )
--- src/constants.py
+++ src/constants.py
@@ -13,11 +13,19 @@ class IMDb_Constants():
 
     TV_SERIES_IDENTIFIER = "TV Series"
 
+    # IMDb homepage
     SEARCH_BAR_CSL = "#navbar-query"
     SEARCH_RESULT_TABLE_XPATH = "/html/body/div[1]/div/div[2]/div/div[1]/div/div[2]/table"
     SEARCH_RESULT_FIRST_FULL_BOX_CSL = "div.findSection:nth-child(3) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)"
     SEARCH_RESULT_FIRST_URL_CSL = "div.findSection:nth-child(3) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > a:nth-child(1)"
 
+    # TV Series homepage
     SERIES_HEADER_CSL = ".title_wrapper > h1:nth-child(1)"
     OVERALL_RATINGS_CSL = ".ratingValue > strong:nth-child(1) > span:nth-child(1)"
     SEASONS_COUNT_CSL = ".seasons-and-year-nav > div:nth-child(4) > a:nth-child(1)"
+    EPISODE_GUIDE_DIV_CSL = ".bp_content"
+
+    # TV Series episode guide page
+    SEASONS_DROPDOWN_CSL = "#bySeason"
+    SEASONS_DROPDOWN_ID = "bySeason"
+    EPISODES_GUIDE_IDENTIFIER = "episodes?ref_=tt_ov_epl"
