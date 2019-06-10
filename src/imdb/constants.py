7de006b7fadda700cbe5241648f6fa3bb8928758
vic-lee <viclee@nyu.edu>
2019-06-10 20:33:31 +0100

    Add new search result xpath

--- src/imdb/constants.py
+++ src/imdb/constants.py
@@ -19,6 +19,7 @@ class IMDb_Constants():
     SEARCH_RESULT_TABLE_XPATH = "/html/body/div[1]/div/div[2]/div/div[1]/div/div[2]/table"
     SEARCH_RESULT_FIRST_FULL_BOX_CSL = "div.findSection:nth-child(3) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)"
     SEARCH_RESULT_FIRST_URL_CSL = "div.findSection:nth-child(3) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > a:nth-child(1)"
+    SEARCH_RESULT_FIRST_URL_XPATH = "//*[@id=\"main\"]/div/div[2]/table/tbody/tr[1]/td[2]/a"
 
     # TV Series homepage
     SERIES_HEADER_CSL = ".title_wrapper > h1:nth-child(1)"
