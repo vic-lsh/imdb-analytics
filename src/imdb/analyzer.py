c03c745312e037fc7e3ee5b5db73a1e173332748
vic-lee <viclee@nyu.edu>
2019-06-10 20:39:11 +0100

    Formatting change

--- src/imdb/analyzer.py
+++ src/imdb/analyzer.py
@@ -8,7 +8,8 @@ import time
 from typing import List
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException, \
+    StaleElementReferenceException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
