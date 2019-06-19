fd6f42d26a026712106ffa8f9690da3cfe87e6c9
vic-lee <viclee@nyu.edu>
2019-06-19 16:40:03 -0400

    Remove legacy methods

--- src/run.py
+++ src/run.py
@@ -2,31 +2,30 @@ import logging
 import logging.config
 import pickle
 import os
+from os.path import abspath
 import yaml
 
+from mongoengine import connect
+
+from db.models import TVSeries, SeasonRatings, EpisodeRating
 from imdb.analyzer import IMDb_Queries_Manager
 from imdb.config import AnalyzerConfig
-from imdb.ratings import SeriesRatingsCollection
+from imdb.ratings import SeriesRatingsCollection, SeriesRatings
 from common.utils import timer
 
-with open('config_logger.yml', 'r') as f:
+with open(abspath('src/config_logger.yml'), 'r') as f:
     cfg = yaml.safe_load(f.read())
 logging.config.dictConfig(cfg)
 
 logger = logging.getLogger(__name__)
 
 
-def analyze(manager, config):
-    manager.add_multiple_queries(config.tv_series_names)
-    print(manager.pending_queries)
-    manager.execute()
-
-
-@timer(in_seconds=True)
+@timer
 def main():
     config = AnalyzerConfig()
     manager = IMDb_Queries_Manager(config)
-    analyze(manager, config)
+    manager.add_multiple_queries(config.tv_series_names)
+    manager.db_execute()
 
 
 if __name__ == "__main__":
