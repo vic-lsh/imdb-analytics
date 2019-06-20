37c973b0a7af21b263087bd1fe0cb3ad27d4bf1c
vic-lee <viclee@nyu.edu>
2019-06-20 00:14:56 -0400

    Maintain a singleton db connection

--- src/api/app.py
+++ src/api/app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+import mongoengine
+
+from flask import Flask, g
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+
+def get_db():
+    """Maintains a single database connection throughout the app lifetime.
+    Creates a new db connection if one does not already exist.
+    """
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = mongoengine.connect(db='imdb')
+    return db
+
 
 class HelloWorld(Resource):
     def get(self):
