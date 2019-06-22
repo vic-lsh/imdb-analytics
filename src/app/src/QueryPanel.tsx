e79bf105bc985fab98c58a899880366eef38ef5b
vic-lee <viclee@nyu.edu>
2019-06-21 20:55:48 -0400

    Add change handlers

--- src/app/src/QueryPanel.tsx
+++ src/app/src/QueryPanel.tsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './QueryPanel.css';
 
-export default class QueryPanel extends Component<{}, {}> {
+type QueryPanelProps = {}
+
+type QueryPanelState = {
+  query: string
+}
+
+export default class QueryPanel extends Component<QueryPanelProps, QueryPanelState> {
+
+  constructor(props: QueryPanelProps) {
+    super(props);
+    this.state = {
+      query: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    this.setState({ query: event.target.value });
+  }
+
+  handleSubmit(event: any) {
+    event.preventDefault();
+  }
+
 
   render() {
     return (
       <form className="search-form">
         <label className="input-label">
           <h1>TV Series</h1>
-          <input className="search-field" type="text" name="name" />
+          <input className="search-field" type="text" name="tv-series"
+            value={this.state.query} onChange={this.handleChange} />
         </label>
         <input className="submit-btn" type="submit" value="Submit" />
       </form>
