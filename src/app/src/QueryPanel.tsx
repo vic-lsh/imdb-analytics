2d05c243d0d6036fd69ceb7e20c433e1324ad162
vic-lee <viclee@nyu.edu>
2019-06-28 11:14:57 -0400

    Bug fix: remove legacy css import

--- src/app/src/QueryPanel.tsx
+++ src/app/src/QueryPanel.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import './QueryPanel.css';
 import { StyledH1DarkBg } from './Dashboard';
 
 type QueryPanelProps = {
