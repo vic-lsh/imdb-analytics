d50108bfa864ff74452b261c11c374c2190f69eb
vic-lee <viclee@nyu.edu>
2019-07-01 18:04:36 -0400

    Add input output channel support in main

--- src/service-extractor/api/main.go
+++ src/service-extractor/api/main.go
@@ -8,10 +8,23 @@ import (
 	"github.com/vic-lee/site-analyzer/src/service-extractor/api/job"
 )
 
-func main() {
-	r := job.Routes()
+func router(in chan job.TVSeriesExtractionJob, out chan job.TVSeriesExtractionJob) {
+	r := job.Routes(in, out)
 
 	var PORT = 4000
 	fmt.Printf("Router running on port %d\n", PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), r))
+}
+
+func main() {
+	jobs := make(map[int]job.TVSeriesExtractionJob)
+
+	in := make(chan job.TVSeriesExtractionJob)
+	out := make(chan job.TVSeriesExtractionJob)
+
+	go router(in, out)
+
+	for job := range out {
+		jobs[job.ID] = job
+	}
 }
