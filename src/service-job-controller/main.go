264e2e05e129a878dfc3bb5183de847de7accf32
Vic Lee <viclee@nyu.edu>
2019-07-05 22:05:54 -0400

    Update port number

--- src/service-job-controller/main.go
+++ src/service-job-controller/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"net/http"
-	"os/exec"
 	"sync"
 	"time"
 
@@ -15,7 +13,7 @@ import (
 func router(in chan interface{}, out chan *job.ExtractionJob) {
 	r := job.Routes(in, out)
 
-	var PORT = 4000
+	var PORT = 3777
 	fmt.Printf("Router running on port %d\n", PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), r))
 }
@@ -44,21 +42,37 @@ func sendJob(jobs map[int]*job.ExtractionJob, in chan interface{}) {
 }
 
 func extractorExecutesJob(jobs map[int]*job.ExtractionJob, id int) {
-	const pyScriptName = "run.py"
-	const dir = "../extractor"
+	// const pyScriptName = "run.py"
+	// const dir = "../extractor"
 
-	cmd := exec.Command("python3", pyScriptName, jobs[id].Name)
-	cmd.Dir = dir
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
+	// cmd := exec.Command("python3", pyScriptName, jobs[id].Name)
+	// cmd.Dir = dir
+	// var stderr bytes.Buffer
+	// cmd.Stderr = &stderr
 
-	if err := cmd.Run(); err != nil {
-		jobs[id].Status = job.CompletedFailed
-		fmt.Printf("Failed posting %s", jobs[id].Name)
-	} else {
-		jobs[id].Status = job.CompletedSucceeded
-		fmt.Printf("POSTED %s\n", jobs[id].Name)
+	// if err := cmd.Run(); err != nil {
+	// 	jobs[id].Status = job.CompletedFailed
+	// 	fmt.Printf("Failed posting %s", jobs[id].Name)
+	// } else {
+	// 	jobs[id].Status = job.CompletedSucceeded
+	// 	fmt.Printf("POSTED %s\n", jobs[id].Name)
+	// }
+
+	req, err := http.NewRequest("POST", "http://127.0.0.1:5000/", nil)
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	q := req.URL.Query()
+	q.Add("name", jobs[id].Name)
+	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL.String())
+
+	resp, err := http.Post(req.URL.String(), "application/json", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(resp.Body)
 }
 
 func processJobs(jobs map[int]*job.ExtractionJob, jobsPending *[]int) {
