f0b20dce1d833ccf8010c58c50c480756d0ad3f9
Vic Lee <viclee@nyu.edu>
2019-07-06 16:42:41 -0400

    Add script that loads sample data

---
+++ demo.sh
@@ -0,0 +1,31 @@
+# demo.sh
+#
+# This script sets up some data for the front-end to display.
+# 
+# For this script to function properly, start up the service containers first.
+
+echo "
+===============================================================================
+
+    Loading sample data
+    Depending on your network condition, this may take several minutes.
+
+===============================================================================
+"
+
+declare -a names=("Game+of+Thrones" 
+                  "How+I+met+Your+Mother" 
+                  "Black+Mirror"
+                  "Breaking+Bad"
+                  "Friends"
+                  "The+Simpsons"
+                  "The+Office"
+                  "Stranger+Things"
+                  "Futurama"
+                  "Big+Little+Lies")
+
+
+for i in "${names[@]}"
+do
+    curl -X POST "http://localhost:3778/jobs?name=${i}"
+done
