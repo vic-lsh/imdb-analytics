e29d6c78fbb4cf37d32c52cfa2e9b6b6c81fed53
Vic Lee <viclee@nyu.edu>
2019-07-06 21:45:31 -0400

    Misc: remove extra space

--- demo.sh
+++ demo.sh
@@ -26,7 +26,7 @@ which -s brew
 if [[ $? != 0 ]] ; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-    brew update  > /dev/null
+    brew update > /dev/null
 fi
 
 
