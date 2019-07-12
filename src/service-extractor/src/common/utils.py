eb15648df748a06297819b3a11dc2a8ed1adbdbc
Vic Lee <viclee@nyu.edu>
2019-07-11 23:58:03 -0400

    Add docstrings for `utils.py`

--- src/service-extractor/src/common/utils.py
+++ src/service-extractor/src/common/utils.py
@@ -1,4 +1,13 @@
+"""
+common/utils.py
+---------------
+
+Contains project-wide utility functions.
+"""
+
 import os
 
-def get_logger_cfg_fpath():
+
+def get_logger_cfg_fpath() -> str:
+    """Returns the absolute path to logger config"""
     return ''.join([os.getcwd(), '/cfg/logger_cfg.ini'])
