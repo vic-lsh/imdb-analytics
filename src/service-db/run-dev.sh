801314c091f6619701f3a564d0808015017fb347
Vic Lee <viclee@nyu.edu>
2019-07-27 10:26:45 -0400

    Use dev and prod run files for DB Service

---
+++ src/service-db/run-dev.sh
@@ -0,0 +1,5 @@
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=9000
+flask run
--- src/service-db/run-prod.sh
+++ src/service-db/run-prod.sh
@@ -1,5 +1,4 @@
 export FLASK_APP=app.py
-export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=80
+export FLASK_RUN_PORT=9000
 flask run
