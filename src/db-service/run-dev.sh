ea308fe61f78fd8f855cffa6fae6adf77631fa77
Vic Lee <viclee@nyu.edu>
2019-07-28 21:29:58 -0400

    Rename folders

--- src/db-service/run-dev.sh
+++ src/db-service/run-dev.sh
@@ -2,4 +2,5 @@ export FLASK_APP=app.py
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=9000
-flask run
+# Use no reload to suppress reloader's default behavior to run the code twice
+flask run --no-reload
