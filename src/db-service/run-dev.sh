2f7adc3a7314de4d78cde015e634def670003c3d
Vic Lee <viclee@nyu.edu>
2019-07-29 22:08:19 -0400

    Move codebase into /src

--- src/db-service/run-dev.sh
+++ src/db-service/run-dev.sh
@@ -1,6 +1,5 @@
-export FLASK_APP=app.py
+export FLASK_APP=src/app.py
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=9000
-# Use no reload to suppress reloader's default behavior to run the code twice
-flask run --no-reload
+flask run
--- src/db-service/run-prod.sh
+++ src/db-service/run-prod.sh
@@ -1,4 +1,4 @@
-export FLASK_APP=app.py
+export FLASK_APP=src/app.py
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=9000
 flask run
